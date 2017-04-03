07a99254b4b195cf59dd20bc14730d25b71dd6e5
Matt Weldon <matt@mattweldon.com>
2017-04-03 14:55:59 +0100

    Feature: Use select tag for component

--- addon/components/tiny-select.js
+++ addon/components/tiny-select.js
@@ -6,32 +6,31 @@ export default Ember.Component.extend({
   prompt: null,
   optionValuePath: 'value',
   optionLabelPath: 'label',
-
+  tagName: 'select',
   init: function () {
     this._super(...arguments);
     if (!this.get('content')) {
       this.set('content', []);
     }
   },
+  change: function() {
+    let selectedIndex = this.$()[0].selectedIndex;
+    let options = this.get('options');
 
-  actions: {
-    change: function() {
-      let selectedIndex = this.$('select')[0].selectedIndex;
-      let options = this.get('options');
-
-      // decrement index by 1 if we have a prompt
-      let hasPrompt = !!this.get('prompt');
-      let optionIndex = hasPrompt ? selectedIndex - 1 : selectedIndex;
-      let _selection = options[optionIndex];
-
-      this.sendAction('willChangeAction', _selection);
-      if (this.get('optionValuePath')) {
-        this.set('selection', _selection[this.get('optionValuePath')]);
-      } else {
-        this.set('selection', _selection);
-      }
-
-      this.sendAction('didChangeAction', _selection);
+    // decrement index by 1 if we have a prompt
+    let hasPrompt = !!this.get('prompt');
+    let optionIndex = hasPrompt ? selectedIndex - 1 : selectedIndex;
+    let _selection = options[optionIndex];
+    
+    this.sendAction('willChangeAction', _selection);
+    if (this.get('optionValuePath')) {
+      this.set('selection', _selection[this.get('optionValuePath')]);
+    } else {
+      this.set('selection', _selection);
     }
-  }
+
+    this.sendAction('didChangeAction', _selection);
+  },
 });
+
+// <select disabled={{disabled}} class="{{class}}" {{action 'change' on='change'}}>
